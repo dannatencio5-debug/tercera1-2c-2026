efa19e1fc45e7bbbebac4ed18e4fd46e5dcc570a
dannatencio5-debug <dannatencio5@gmail.com>
2026-06-07 22:25:01 -0600

    leccion 3

---
+++ playstation.php
@@ -0,0 +1,5 @@
+<?php require_once "./vistas/vista_superior.php"?>
+
+<h1>playstation</h1>
+
+<?php require_once "./vistas/vista_inferior.php"?>
---
+++ vistas/vista_superior.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style/main.css">
+    <title>tercera1</title>
+   
+ </head>
+   <body>
+     <menu>
+        <div class="menu-izquierdo">
+            <img src="./img/menu.png" alt="menu principal">
+        </a>
+      </div>
+     <div class="menu-derecha">
+        <a href="./index.php">inicio</a>
+        <a href="./xbox.php">xbox</a>
+        <a href="./nintendo.php">nintendo</a>
+        <a href="./playstation.php">playstation</a>
+    </div>
+        
+     <menu>
